351ed5f6b3d3aa688c8654ef8aaf7b91fc1d251c
notyy <notyycn@gmail.com>
2018-05-23 17:54:23 +0800

    insert 10M data into h2db

--- scripts/DBSetup.scala
+++ scripts/DBSetup.scala
@@ -3,11 +3,16 @@ import biz.repo.CustomerRepo
 import biz.{DBConfigProvider, Database}
 import com.typesafe.scalalogging.StrictLogging
 
+/**
+  * each time this object is run, 10M customer data will
+  * be inserted into database, so DON'T run it often
+  */
 object DBSetup extends App with StrictLogging {
   logger.info("create database")
   val h2fileDB = new Database with DBConfigProvider {
-    override def dbName: String = "h2mem1"
+    override def dbName: String = "h2file"
   }
+  h2fileDB.dropDB()
   h2fileDB.setupDB()
   logger.info("database created")
 
@@ -15,7 +20,7 @@ object DBSetup extends App with StrictLogging {
     override val database = h2fileDB
   }
 
-  val i = 100
+  val i = 10000000
   logger.info(s"insert $i data into customer table")
   (1 to 100).foreach { i =>
     val customer = Customer(None, s"yy$i",Some("20180523"),Some("some address on earth"))
